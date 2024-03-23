a5fc081c1e10fadc168f7e4e0e2617dadce28522
lefteris <lefteris@alexakis.org>
2024-03-23 04:54:44 +0100

    added z

--- home.nix
+++ home.nix
@@ -38,7 +38,21 @@ in
       g = "git";
       lg = "lazygit";
       lll = "ls -lrth";
-  };
+    };
+    plugins = [
+      # Enable a plugin (here grc for colorized command output) from nixpkgs
+      { name = "grc"; src = pkgs.fishPlugins.grc.src; }
+      # Manually packaging and enable a plugin
+      {
+        name = "z";
+        src = pkgs.fetchFromGitHub {
+          owner = "jethrokuan";
+          repo = "z";
+          rev = "e0e1b9dfdba362f8ab1ae8c1afc7ccf62b89f7eb";
+          sha256 = "0dbnir6jbwjpjalz14snzd3cgdysgcs3raznsijd6savad3qhijc";
+        };
+      }
+    ];
 };
     
 
