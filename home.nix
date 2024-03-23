91f0e6d5aa9afd7f30071c836f939e3c00168047
lefteris <lefteris@alexakis.org>
2024-03-23 05:58:18 +0100

    add a fish shell function

--- home.nix
+++ home.nix
@@ -52,7 +52,33 @@ in
           sha256 = "0dbnir6jbwjpjalz14snzd3cgdysgcs3raznsijd6savad3qhijc";
         };
       }
+     { name = "tide"; src = pkgs.fishPlugins.tide; }
+     { name = "git"; src = pkgs.fishPlugins.plugin-git; }
     ];
+   functions = {
+      groot = {
+        description = "cd to the root of the current git repository";
+        body = ''
+          set -l git_repo_root_dir (git rev-parse --show-toplevel 2>/dev/null)
+          if test -n "$git_repo_root_dir"
+            cd "$git_repo_root_dir"
+            echo -e ""
+            echo -e "      \e[1m\e[38;5;112m\^V//"
+            echo -e "      \e[38;5;184m|\e[37m· ·\e[38;5;184m|      \e[94mI AM GROOT !"
+            echo -e "    \e[38;5;112m- \e[38;5;184m\ - /"
+            echo -e "     \_| |_/\e[38;5;112m¯"
+            echo -e "       \e[38;5;184m\ \\"
+            echo -e "     \e[38;5;124m__\e[38;5;184m/\e[38;5;124m_\e[38;5;184m/\e[38;5;124m__"
+            echo -e "    |_______|"
+            echo -e "     \     /"
+            echo -e "      \___/\e[39m\e[00m"
+            echo -e ""
+          else
+            echo "Not in a git repository."
+          end
+        '';
+      };
+   };
 };
     
 
