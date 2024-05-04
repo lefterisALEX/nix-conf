f893e662c76856cc13c2c2b20d25ef0e203db998
lefteris <lefteris@alexakis.org>
2024-05-04 18:24:28 +0000

    module zsh

--- modules/home/home.nix
+++ modules/home/home.nix
@@ -31,26 +31,6 @@ in
     options = [ "once" "fzf" ];
   };
 
-#  programs.zsh = {
-#    enable = true;
-#    enableAutosuggestions = true;
-#    enableCompletion = false;
-#    oh-my-zsh = {
-#      enable = true;
-#      theme = "robbyrussell";
-#      plugins = [
-#        "git"
-#        "npm"
-#        "history"
-#        "node"
-#        "rust"
-#        "deno"
-#      ];
-#    };
-#    autocd = true;
-#    dotDir = ".config/zsh";
-#};
-
 
   home.packages = with pkgs; ([
     # Common packages
