f80568bb2ea10eb3bb0940996c166a78c021134a
lefteris <lefteris@alexakis.org>
2024-05-05 04:29:50 +0000

    add them for fish

---
+++ modules/home/config.fish
@@ -0,0 +1,29 @@
+# Felix' Fish Configuration Init File.
+
+# Fix a problem with paging on nixos, will be resolved once fish version is
+# updated. https://github.com/NixOS/nixpkgs/issues/85158
+
+set PAGER less
+set LESS "-R"
+
+
+function awaketime -d "Display time since last waking."
+    echo "Awake Since " \
+    (pmset -g log | awk -e '/ Wake  /{print $2}' | tail -n 1)
+end
+
+
+function gitlog -d "More detailed, prettified output for git."
+    git log --graph --abbrev-commit \
+    --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset"
+end
+
+
+function ghci-nix -d "Nix shell with haskell and packages."
+    set PCKS ""
+    for s in $argv
+        set PCKS "$s $PCKS"
+    end
+    nix-shell -p "haskellPackages.ghcWithPackages (ps: with ps; [ $PCKS ])" \
+    --command ghci
+end
--- modules/home/fish.nix
+++ modules/home/fish.nix
@@ -1,6 +1,32 @@
  { pkgs, ... }: { 
   programs.fish = {
     enable = true;
+    shellInit = builtins.readFile ./config.fish;
+
+    plugins = [
+      # Enable a plugin (here grc for colorized command output) from nixpkgs
+      { name = "grc"; src = pkgs.fishPlugins.grc.src; }
+      #{ name = "tide"; src = pkgs.fishPlugins.tide.src; }
+      # Manually packaging and enable a plugin
+      {
+        name = "z";
+        src = pkgs.fetchFromGitHub {
+          owner = "jethrokuan";
+          repo = "z";
+          rev = "e0e1b9dfdba362f8ab1ae8c1afc7ccf62b89f7eb";
+          sha256 = "0dbnir6jbwjpjalz14snzd3cgdysgcs3raznsijd6savad3qhijc";
+        };
+      }
+      {
+        name = "bobthefisher";
+        src = pkgs.fetchFromGitHub {
+          owner = "Scrumplex";
+          repo = "bobthefisher";
+          rev = "f4179a14b087c7fbfc2e892da429adad40a39e44";
+          sha256 = "sha256-l1DHSZa40BX2/4GCjm5E53UOeFEnMioWbZtCW14WX6k=";
+        };
+      }
+    ];
 
     shellAliases = {
       gogit = "cd ~/git";
