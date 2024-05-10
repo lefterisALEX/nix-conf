f47b60dad8bee779cae2304a6e4c5915a0b39f59
lefteris <lefteris@alexakis.org>
2024-05-10 04:23:51 +0000

    cleanup

--- encrypt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Define the paths to the old/current and new age key files
-SOPS_AGE_KEY_FILE=~/.config/sops/age/keys.txt
-SOPS_AGE_KEY_FILE_NEW=./age.key
-nix-shell -p sops age --run  "sops -d modules/home/work.fish.enc"
