830807f3da7b8187507b9ff2969b37c1d6342396
lefteris <lefteris@alexakis.org>
2024-06-16 12:07:39 +0200

    feat(keymaps) add keymap for move between splits and find open buffers

--- modules/home/nvim/lua/josean/core/keymaps.lua
+++ modules/home/nvim/lua/josean/core/keymaps.lua
@@ -14,6 +14,7 @@ keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decremen
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sw", "<C-w>w", { desc = "Move between splits" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
