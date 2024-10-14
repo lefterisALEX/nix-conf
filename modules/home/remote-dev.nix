eec215e30a6ac6e30136472f1a5a9a15f33f6307
lefteris alexakis <lefteris.alexakis@kpn.com>
2024-10-15 00:41:58 +0200

    fix(shell) remove hostname from remote-dev file

--- modules/home/remote-dev.nix
+++ modules/home/remote-dev.nix
@@ -8,11 +8,6 @@ in
   imports = [
    ];
 
-  home.username = "lefteris";
-  home.homeDirectory =
-    if isLinux then "/home/lefteris" else
-    if isDarwin then "/Users/lefteris" else unsupported;
-
   home.stateVersion = "23.11"; # Don't change this. This will not upgrade your home-manager.
 #  programs.home-manager =  {
 #     enable = true;
