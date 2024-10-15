5ceef256da41c64096f503f24c79f578f0ab9b0c
lefteris alexakis <lefteris.alexakis@kpn.com>
2024-10-15 13:08:04 +0200

    fix(linux-server) add support for work0functions

--- modules/home/fish.nix
+++ modules/home/fish.nix
@@ -27,6 +27,9 @@ in
         if test -e /Users/lefteris/work-functions;
             source /Users/lefteris/work-functions/work.fish
         end
+        if test -e /home/lefteris/work-functions;
+            source /home/lefteris/work-functions/work.fish
+        end
 
         # Conditionally add Homebrew environment setup if on macOS
         ${lib.optionalString isDarwin ''
